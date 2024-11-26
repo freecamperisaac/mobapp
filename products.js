66f2c64795c0f9cb1944100fcd8c9fc9f4528b8d
Isaac Osei Owusu <isaacjohnson672@gmail.com>
2024-11-26 15:54:49 +0400

    added files

---
+++ products.js
@@ -0,0 +1,38 @@
+let products = [
+    {
+      "id": 1002,
+      "title": "Yarn",
+      "description": "Yarn your cat can play with for a very <strong>long</strong> time!",
+      "price": 2.99,
+      "image": "images/basketball.png",
+      "availableInventory": 7,
+      "rating": 3
+    },
+    {
+      "id": 1003,
+      "title": "Kitty Litter",
+      "description": "Premium kitty litter for your cat.",
+      "price": 11.00,
+      "image": "images/football.png",
+      "availableInventory": 99,
+      "rating": 4
+    },
+    {
+      "id": 1004,
+      "title": "Cat House",
+      "description": "A place for your cat to play!",
+      "price": 7.99,
+      "image": "images/music.png",
+      "availableInventory": 11,
+      "rating": 5
+    },
+    {
+      "id": 1005,
+      "title": "Laser Pointer",
+      "description": "Drive your cat crazy with this <em>amazing</em> product.",
+      "price": 49.99,
+      "image": "images/padel.png",
+      "availableInventory": 25,
+      "rating": 4
+    }
+  ]
